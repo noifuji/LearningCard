8cac77c0b86adbd64ba07f9ba9378fa00827912f
noifuji <ryoma1025@gmail.com>
2016-06-27 02:42:16 +0900

    installed jacoco

--- app/src/test/java/jp/noifuji/learningcard/logic/LogicExecutorTest.java
+++ app/src/test/java/jp/noifuji/learningcard/logic/LogicExecutorTest.java
@@ -2,11 +2,14 @@ package jp.noifuji.learningcard.logic;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created by ryoma on 2016/06/26.
  */
 public class LogicExecutorTest {
     @Test
     public void sample() throws Exception {
+        assertTrue(true);
     }
 }
